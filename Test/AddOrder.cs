f88dc0c0cb010dc0f20b5f404be040422d63f9c0
AkifCan38 <148538864+AkifCan38@users.noreply.github.com>
2023-12-09 15:07:32 +0600

    Add files via upload

---
+++ Test/AddOrder.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using OrderMS;
+
+namespace Test {
+    public partial class AddOrder : Form
+    {
+        private readonly Panel panel;
+        private readonly bool newOrder = true;
+        private readonly string id;
+
+
+        public AddOrder(Panel panel)
+        {
+            InitializeComponent();
+
+            OrderState.SelectedIndex = 0;
+
+            this.panel = panel;
+            this.newOrder = true;
+            this.deleteOrder.Visible = false;
+            this.tableLayoutPanel3.ColumnCount = 1;
+        }
+
+        public AddOrder(string id, Panel panel)
+        {
+            InitializeComponent();
+
+            // Initialize Class Values
+            this.panel = panel;
+            this.newOrder = false;
+            this.id = id;
+            this.Text = "Order (No: " + id + ")";
+
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=Database.db"))
+            {
+                connection.Open();
+                string query = "SELECT * FROM tblOrder WHERE Id = " + id;
+
+                using (SQLiteCommand action = new SQLiteCommand(query, connection))
+                {
+                    action.ExecuteNonQuery();
+
+                    using (SQLiteDataReader data = action.ExecuteReader())
+                    {
+                        // Set SQL Values to Form Inputs
+                        if (data.Read())
+                        {
+                            CustomerNameInput.Text = data["Customer"].ToString();
+                            NotesInput.Text = data["Notes"].ToString();
+                            OrderInput.Text = data["OrderDetails"].ToString();
+                            AddressInput.Text = data["Address"].ToString();
+
+                            PriceInput.Value = Convert.ToInt32(data["Payment"]);
+
+                            OrderState.SelectedIndex = Int32.Parse(data["State"].ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        private void addOrderBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                TextBox[] Inputs = { CustomerNameInput, AddressInput, OrderInput };
+
+                if (PriceInput.Value.ToString() == null || PriceInput.Value.ToString() == "0")
+                {
+                    throw new Exception("Required fields cannot be left blank!");
+                }
+
+                for (int i = 0; i < Inputs.Length; i++)
+                {
+                    string text = Inputs[i].Text;
+
+                    if (string.IsNullOrWhiteSpace(text))
+                    {
+                        Inputs[i].Focus();
+                        throw new Exception("Required fields cannot be left blank!");
+                    }
+                }
+
+                Crud.AddOrder(this.newOrder, CustomerNameInput.Text, OrderInput.Text, AddressInput.Text, NotesInput.Text, PriceInput.Text, OrderState.SelectedIndex, this.id);
+
+                this.panel.UpdateTable();
+                this.panel.Focus();
+                Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "An error has occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void deleteOrder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Crud.DeleteOrderById(this.id);
+
+                MessageBox.Show("Order Deleted!");
+                this.panel.UpdateTable();
+                this.panel.Focus();
+                Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "An error has occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OrderInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OrderState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Test/UserManagmentForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using OrderMS;
+
+namespace OrderMS {
+    public partial class UserManagmentForm : Form
+    {
+        public UserManagmentForm()
+        {
+            InitializeComponent();
+            UpdateTable();
+        }
+
+        public void UpdateTable()
+        {
+
+            try
+            {
+                using SQLiteConnection connection = new SQLiteConnection("Data Source=Database.db");
+                connection.Open();
+
+                // Return Orders List ordered by their states
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter("Select * From tblUsers", connection);
+                DataSet dset = new DataSet();
+                adapter.Fill(dset, "info");
+
+
+                UsersTable.DataSource = dset.Tables[0];
+
+                adapter.Dispose();
+                connection.Close();
+
+                // Remove table selection
+                UsersTable.ClearSelection();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void UsersTable_DataSourceChanged(object sender, EventArgs e)
+        {
+            // Rename Table Header Texts
+            UsersTable.Columns[0].HeaderText = "ID";
+            UsersTable.Columns[1].HeaderText = "User Name";
+            UsersTable.Columns[2].HeaderText = "Password";
+            UsersTable.Columns[3].HeaderText = "Authorization";
+
+            // Convert States into Text
+            for (int i = 0; i < UsersTable.Rows.Count; i++)
+            {
+                int state = Convert.ToInt32(UsersTable.Rows[i].Cells[3].Value);
+                UsersTable.Rows[i].Cells[3].Value = GetState(state);
+            }
+        }
+
+        private string GetState(int state)
+        {
+            // Order States
+            var stateMap = new Dictionary<int, string>(){
+        {0, "User"},
+        {1, "Executive"},
+      };
+
+            string output;
+
+            return stateMap.TryGetValue(state, out output) ? output : "default";
+        }
+
+        private void UsersTable_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to delete the user?", "Delete", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+
+            if (result == DialogResult.OK)
+            {
+                string id = UsersTable.Rows[e.RowIndex].Cells[0].Value.ToString();
+                Crud.DeleteUserById(id);
+                UpdateTable();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = UsernameInput.Text;
+            string password = PasswordInput.Text;
+            bool isAdmin = isAdminCB.Checked;
+
+            try
+            {
+                using SQLiteConnection connection = new SQLiteConnection("Data Source=Database.db");
+                connection.Open();
+
+                string action = "INSERT INTO tblUsers(username, password, type) values(@username, @password, @type)";
+
+                SQLiteCommand actionRun = new SQLiteCommand(action, connection);
+
+                actionRun.Parameters.AddWithValue("@username", username);
+                actionRun.Parameters.AddWithValue("@password", password);
+                actionRun.Parameters.AddWithValue("@type", Convert.ToInt32(isAdmin).ToString());
+
+                actionRun.ExecuteNonQuery();
+
+                actionRun.Dispose();
+                connection.Close();
+
+                UsernameInput.Text = "";
+                PasswordInput.Text = "";
+                isAdminCB.Checked = false;
+
+                UpdateTable();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "An error has occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UserManagmentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PasswordInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsernameInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
